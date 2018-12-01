1f50f64072fe5c8fbbdd9f9c1a43ba971797149b
Meshal <Meshal@MeshalPC>
2018-12-01 21:24:51 +0300

    Working on LongTermScheduler

--- src/LongTermScheduler.java
+++ src/LongTermScheduler.java
@@ -1,7 +1,62 @@
 
 public class LongTermScheduler {
-	
+	public final static int ramSize = 192;
+	// private JobQueue jobQueue ;
+	private int availableSize;
+	public PQ<PCB> readyQueue;
+	public PQ<PCB> waitingQueue;
+	public PQ<PCB> jobQueue;
 
+	public LongTermScheduler() {
+		this.jobQueue = JobQueue.getProcesses();
+		this.availableSize = 144;
+		this.readyQueue = new PQ();
+		this.waitingQueue = new PQ();
+	}
 
+	public PQ loadToReadyQueue() {
+		PQ<PCB> tempQ = new PQ();
+		// check if both Queue empty
+		if (jobQueue.length() <= 0 && waitingQueue.length() <= 0) {
+			return this.readyQueue;
+		}
+		// Check Waiting queue first
+		while (waitingQueue.length() > 0) {
+
+			PCB process = waitingQueue.serve().data;
+
+			if (process.getMemory() <= availableSize) {
+				availableSize = availableSize - process.getMemory();
+				readyQueue.enqueueMin(process, process.getCpuBurst());
+			} else {
+				tempQ.enqueueMin(process, 1); // to cycle through waiting queue
+			}
+
+		}
+		// return waiting Queue
+		while (tempQ.length() != 0)
+			waitingQueue.enqueueMin(tempQ.serve().data, 1);
+		PCB process = jobQueue.serve().data;
+
+		while (jobQueue.length() != 0 && availableSize != 0) {  // if there is enough   
+			if (process.getMemory() <= availableSize) {
+				readyQueue.enqueueMin(process, process.getCpuBurst());
+				availableSize = availableSize - process.getMemory();
+			}else{
+				waitingQueue.enqueueMin(process, 1);
+			}
+			
+			process = jobQueue.serve().data;
+
+		}
+		if (availableSize == 0)
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	}
 
 }
