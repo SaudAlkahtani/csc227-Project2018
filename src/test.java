64c256abe9cb931d590032cfb110e7f9fc2e2b8e
Saud amer Alkahtani <saud.a.kahtani@gmail.com>
2018-12-02 18:11:48 +0300

    Test class with normal print method

--- src/test.java
+++ src/test.java
@@ -1,34 +1,45 @@
+import java.io.FileNotFoundException;
 
 public class test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 
 		PQ q1 = new PQ();
 		PQ q2 = new PQ();
-
-		q1.enqueueMax("Process 1 ", 1);
-		q1.enqueueMax("Process 2 ", 1);
-		q1.enqueueMax("Process 3 ", 1);
-		q1.enqueueMax("Process 4 ", 1);
-		q1.enqueueMax("Process 5 ", 1);
-		q1.enqueueMax("Process 6 ", 1);
-		q1.enqueueMax("Process 7 ", 1);
-		q1.enqueueMax("Process 8 ", 1);
 		
+//		q1.enqueueMax("Process 1 ", 1);
+//		q1.enqueueMax("Process 2 ", 1);
+//		q1.enqueueMax("Process 3 ", 1);
+//		q1.enqueueMax("Process 4 ", 1);
+//		q1.enqueueMax("Process 5 ", 1);
+//		q1.enqueueMax("Process 6 ", 1);
+//		q1.enqueueMax("Process 7 ", 1);
+//		q1.enqueueMax("Process 8 ", 1);
+//		
+		JobQueue j1= new JobQueue();
+		Queue<PCB> process = j1.getProcesses();
+//		System.out.println(Integer.parseInt("0"));
 		
+		while(process.length()!=0){
+			int cycle=1;
+			PCB a = process.serve();
+			System.out.println(a.getPid());
+			Queue<Cycle> Cycles=a.getCycles();
+			while(Cycles.length()!=0) {
+				Cycle c = Cycles.serve();
+				System.out.println("Cylce: " + cycle);
+				System.out.print("Cpu Burst: " + c.getCpuBurst());
+				System.out.print(" Memory: " + c.getMemory());
+				System.out.println(" IO : " + c.getIOBurst());
+				cycle++;
+				
+				
+			}
 		
-		q1.printall();
-		
-		System.out.println("------------------------------------------------------------------------------------------------");
-		
-		while(q1.length()!=0){
-			PQNode a = q1.serve();
-			
-			q2.enqueueMin(a.data, a.priority);
 		}
-		
-		q2.printall();
+//		
+//		q2.printall();
 
 		
 	}
